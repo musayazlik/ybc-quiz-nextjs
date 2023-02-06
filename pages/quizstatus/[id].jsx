9715a75603874f67bd73f6988667b859e8a67301
Musa Yazlık <33998073+musayazlik@users.noreply.github.com>
2023-02-06 22:05:40 +0300

    Cleaning operations have been done

--- pages/quizstatus/[id].jsx
+++ pages/quizstatus/[id].jsx
@@ -4,13 +4,12 @@ import { tr, en, lang } from "@/lang/langT"
 import { get, child, ref } from "firebase/database"
 import { useRouter } from "next/router"
 import { db } from "@/libs/firebase"
+import Swal from "sweetalert2"
 
 const QuizStatus = ({ questionData }) => {
   const router = useRouter()
   const { locale } = router
   const t = lang(locale)
-
-  console.log(questionData)
 
   return (
     <>
@@ -36,16 +35,24 @@ export default QuizStatus
 
 export async function getServerSideProps(context) {
   const { id } = context.query
+  try {
+    const question = await get(child(ref(db), `quizzes`))
+    const questionData = question.val()
+    const questionId = Object.keys(questionData).find(
+      (key) => questionData[key].quizId === id
+    )
 
-  const question = await get(child(ref(db), `quizzes`))
-  const questionData = question.val()
-  const questionId = Object.keys(questionData).find(
-    (key) => questionData[key].quizId === id
-  )
-
-  return {
-    props: {
-      questionData: questionData[questionId],
-    },
+    return {
+      props: {
+        questionData: questionData[questionId],
+      },
+    }
+  } catch (error) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    }
   }
 }
