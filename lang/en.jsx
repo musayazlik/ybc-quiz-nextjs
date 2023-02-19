65b7c918447bf1d215fb28097c278634ba0c7f91
Musa Yazlık <33998073+musayazlik@users.noreply.github.com>
2023-02-19 22:12:31 +0300

    Language packs have been created.

--- lang/en.jsx
+++ lang/en.jsx
@@ -1,11 +1,90 @@
 export default {
   welcome: "Welcome...",
+  start: "Start Quiz",
   quizpagep: `Your lover, spouse or friend <br /><span>to test the <span className="text-red-500  underline decoration-red-500 decoration-6">relationship</span> </span>between you and your partner.`,
   quizpagebutton: "Start a free quiz",
+  quizSolve: {
+    title: "Solve Quiz",
+    subtitle1: "Now it's time to solve a quiz...",
+    subtitle1Edit: "Now it's time to edit the quiz...",
+    subtitle2:
+      "This Quiz consists of multiple choice and open-ended questions.",
+    button: "Start Solving Quiz",
+    error: {
+      title: "Quiz Not Found",
+      subtitle1: "Sorry, this quiz was not found.",
+      subtitle2: "Please try again later.",
+      button: "Back to Quiz",
+    },
+    solved: {
+      title: "Quiz Solved",
+      subtitle: "This quiz has already been solved.",
+      button: "Go to Home",
+      buttonEdit: "Edit Quiz",
 
-  createQuiz: {
-    title: "Quiz Time",
-    subtitle:
-      "Now it's time to create a quiz... In this quiz you can create multiple questions and open questions.",
+      alerts: {
+        error: {
+          title: "Error!",
+          subtitle: "An error occurred. Please try again later.",
+          button: "Okay",
+        },
+        success: {
+          title: "Success!",
+          subtitle: "Quiz solved successfully.",
+          subtitleEdit: "Quiz edited successfully.",
+          button: "Okay",
+        },
+      },
+    },
+  },
+  quizCreate: {
+    title: "Create Quiz",
+    subtitle: "Now it's time to create a quiz...",
+    subtitle2:
+      "This Quiz consists of multiple choice and open-ended questions.",
+    button: "Create Quiz",
+    personalInformation: {
+      title: "Personal Information",
+      subtitle:
+        "This information will be stored only for you and shared only with you.",
+    },
+    yourName: "Your Name",
+    yourPartnerName: "Your Partner's Name",
+    testQuestionPool: {
+      title: "Test Question Pool",
+      subtitle:
+        "Select the questions you want to ask from the question pool below.",
+    },
+    classicQuestions: {
+      title: "Classic Questions",
+      subtitle: "Here you can add classic questions.",
+      warning: "You can add up to 5 custom questions.",
+      addQuestion: "Add",
+    },
+    alerts: {
+      error: {
+        classicMaxFied: {
+          title: "Error!",
+          subtitle: "You can add up to 5 custom questions.",
+          button: "Okay",
+        },
+        allFields: {
+          title: "Error!",
+          subtitle: "Please fill in all fields.",
+          button: "Okay",
+        },
+        emptyFields: {
+          title: "Error!",
+          subtitle:
+            "You cannot add a blank question. Please add your question.",
+          button: "Okay",
+        },
+      },
+      success: {
+        title: "Success!",
+        subtitle: "Quiz created successfully.",
+        button: "Okay",
+      },
+    },
   },
 }
--- lang/tr.jsx
+++ lang/tr.jsx
@@ -1,10 +1,92 @@
 export default {
   welcome: "Hoşgeldiniz...",
+  start: "Quiz'e Başla",
   quizpagep: `Sevgiliniz, Eşiniz veya Arkadaşınız<br /><span> ile aranızdaki <span className="text-red-500  underline decoration-red-500 decoration-6">ilişkiyi</span></span>test edebileceğiniz en iyi uygulamayı yaptık.`,
   quizpagebutton: "Ücretsiz bir quiz başlat",
   createQuiz: {
     title: "Quiz Zamanı",
     subtitle:
       "Şimdi bir quiz oluşturma zamanı...Bu Quiz çoktan sorular ve açık sorular oluşturabilirsiniz.",
+  },
+  quizSolve: {
+    title: "Quiz Çözme",
+    subtitle1: "Şimdi bir quiz çözme zamanı...",
+    subtitle1Edit: "Şimdi quizi düzenleme zamanı...",
+    subtitle2: "Bu Quiz çoktan seçmeli ve açık uçlu sorulardan oluşmaktadır.",
+    button: "Quiz'i Çözmeye Başla",
+    error: {
+      title: "Quiz Bulunamadı",
+      subtitle1: "Üzgünüz, bu quiz bulunamadı.",
+      subtitle2: "Lütfen daha sonra tekrar deneyin.",
+      button: "Quiz'e Geri Dön",
+    },
+    solved: {
+      title: "Quiz Çözüldü",
+      subtitle: "Bu quiz zaten çözülmüş.",
+      button: "Ana Sayfaya Dön",
+      buttonEdit: "Quiz'i Düzenle",
+
+      alerts: {
+        error: {
+          title: "Hata!",
+          subtitle: "Bir hata oluştu. Lütfen daha sonra tekrar deneyin.",
+          button: "Tamam",
+        },
+        success: {
+          title: "Başarılı!",
+          subtitle: "Quiz başarıyla çözüldü.",
+          subtitleEdit: "Quiz başarıyla düzenlendi.",
+          button: "Tamam",
+        },
+      },
+    },
+  },
+  quizCreate: {
+    title: "Quiz Oluştur",
+    subtitle: "Şimdi bir quiz oluşturma zamanı...",
+    subtitle2: "Bu Quiz çoktan seçmeli ve açık uçlu sorulardan oluşmaktadır.",
+    button: "Quiz'i Oluştur",
+    personalInformation: {
+      title: "Kişisel Bilgiler",
+      subtitle:
+        "Bu bilgiler sadece sizin için saklanacak ve sadece sizinle paylaşılacak.",
+    },
+    yourName: "Adınız",
+    yourPartnerName: "Partnerinizin İsmi",
+    testQuestionPool: {
+      title: "Test Soru Havuzu",
+      subtitle:
+        "Aşağıdaki soru havuzundan sormak istediğiniz soruları seçiniz.",
+    },
+    classicQuestions: {
+      title: "Klasik Sorular",
+      subtitle: "Burada klasik sorular ekleyebilirsiniz.",
+      warning: "En fazla 5 soru ekleyebilirsiniz.",
+      addQuestion: "Ekle",
+    },
+    alerts: {
+      error: {
+        classicMaxFied: {
+          title: "Hata!",
+          subtitle: "En fazla 5 özel soru ekleyebilirsiniz.",
+          button: "Tamam",
+        },
+        allFields: {
+          title: "Hata!",
+          subtitle: "Lütfen tüm alanları doldurun.",
+          button: "Tamam",
+        },
+        emptyFields: {
+          title: "Hata!",
+          subtitle: "Boş bir soru ekleyemezsiniz. Lütfen sorunuzu yazınız.",
+          button: "Tamam",
+        },
+      },
+      success: {
+        title: "Başarılı!",
+        subtitle: "Quiz başarıyla oluşturuldu.",
+        button: "Tamam",
+      },
+    },
   },
 }
